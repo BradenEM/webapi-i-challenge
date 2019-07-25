b0eccaf24777bbdc526d2c367a06fc2b12acac55
Braden Mason <bradenemason@gmail.com>
2019-07-24 21:11:15 -0500

    delete is working

--- index.js
+++ index.js
@@ -56,3 +56,21 @@ server.post("/api/users", (req, res) => {
       });
     });
 });
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => [
+      res.status(500).json({ errorMessage: "The user could not be removed" })
+    ]);
+});
