955d6aa9b5759d339feb6035d8c40187df49639a
Braden Mason <bradenemason@gmail.com>
2019-07-24 20:09:34 -0500

    server is running

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(4000, () => {
+  console.log("Server is listening.");
+});
+
+server.get("/", (req, res) => {
+  res.send("Hello from this page");
+});
