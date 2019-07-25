81b678ce1573cadcaff7f814306172465ac6d53c
Braden Mason <bradenemason@gmail.com>
2019-07-24 20:55:01 -0500

    get and post users are working

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ server.listen(4000, () => {
 server.get("/", (req, res) => {
   res.send("Hello from this page");
 });
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status({ error: "The users information could not be retrieved." });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const bodyInfo = req.body;
+  console.log(bodyInfo);
+
+  db.insert(bodyInfo)
+    .then(user => {
+      res.status(303).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "There was an error while saving the user to the database"
+      });
+    });
+});
